d8e3a75fb0aeacb4f3b1f5e12b8ce1d601762aac
jacixn <antwijason55@gmail.com>
2026-01-02 01:30:02 +0000

    restore widget and update to v1.0.24 build 24

---
+++ fivefold-ios/ios/BiblelyVerseWidget/BiblelyVerseWidgetBundle.swift
@@ -0,0 +1,14 @@
+//
+//  BiblelyVerseWidgetBundle.swift
+//  BiblelyVerseWidget
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BiblelyVerseWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BiblelyVerseWidget()
+    }
+}
